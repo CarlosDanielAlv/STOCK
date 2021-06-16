6a94944e3ae76a0c6373e4946c814c788ff788d0
Carlos Daniel da Silva Alves <69328049+CarlosDanielAlv@users.noreply.github.com>
2021-06-16 15:08:48 -0300

    ConnectionFactory, código JDBC para ligação com banco de dados

--- src/BO/ConnectionFactory.java
+++ src/BO/ConnectionFactory.java
@@ -1,8 +1,25 @@
 package BO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
- * @author Carlo
+ * @author Carlos
  */
 public class ConnectionFactory {
+    
+    private String url = "jdbc:mysql://127.0.0.1:3306/dbstock";
+    private String user = "root";
+    private String password = "root";
+    
+    public Connection geConnection(){
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception error) {
+            throw new RuntimeException("Erro na connection factory: " + error.getMessage());
+        }
+    }
     
 }
